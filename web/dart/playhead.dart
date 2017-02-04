ecf2cc18ab95fa081a347407fc7d16f3f3980310
Michael Horn <michael-horn@northwestern.edu>
2017-02-04 08:50:42 -0600

    advance now goes through socket first

--- web/dart/playhead.dart
+++ web/dart/playhead.dart
@@ -73,7 +73,8 @@ class PlayHead {
       Socket source = null;
       while (current != null && current != source) {
         if (source == null) source = current;
-        current = current.parent.advance(this);
+        current = current.advance(this);
+        if (current == source) print("looped");
         if (current != null) {
           current.eval(this);
           if (!current.skipAhead(this)) break;
